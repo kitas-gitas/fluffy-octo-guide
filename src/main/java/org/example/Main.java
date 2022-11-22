d3994f6712d22f1c22aca610d2a41d85e0bcbd8e
Useris Userovicius <prof.mokytojas@gmail.com>
2022-11-22 11:49:56 +0200

    Add sakos pakeitimas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        //SAKOS PAKEITIMAS
+        //SAKOS PAKEITIMAS
+        //SAKOS PAKEITIMAS
+        //SAKOS PAKEITIMAS
+        // SAKOS PAKEITIMAS
+
+
+
     }
 }
